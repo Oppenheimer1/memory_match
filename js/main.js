f82fc3399549bd5f98dfc6b3e8c963ce587738dc
LARS BERG <larsgbr@gmail.com>
2016-11-03 14:35:12 -0500

    6th commit, created main.js and added in variables attempts, high_score and new_high_score

---
+++ js/main.js
@@ -0,0 +1,7 @@
+(function(){
+
+	var attempts = 0; //sets the counter variable to zero as the initial number of tries
+	var high_score = 999; //Starts the board with a default high score of 999
+	var new_high_score = 0; //sets the initial new high score variable
+
+})();
